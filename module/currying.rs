b08a2fa7e085766c8cdebc742fb4cc9dd3ef1a5a
alfinsuryaS <benjaminstwo@gmail.com>
2020-03-21 09:30:10 +0800

    Modified comment

--- module/currying.rs
+++ module/currying.rs
@@ -2,7 +2,8 @@
 
 /** 
  * Currying
- * Currying is the technique of translating the evaluation of a function that takes multiple arguments into evaluating a sequence of functions, each with a single argument. 
+ * Currying is the technique of translating the evaluation of a function that takes multiple arguments 
+ * into evaluating a sequence of functions, each with a single argument. 
 
  Code: 
  add(5, 5, 10)
